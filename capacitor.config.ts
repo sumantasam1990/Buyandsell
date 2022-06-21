3cc7837fbb154c23a52dc84bfb943aa490f35f15
Sumanta Kundu <sumantasam1990@gmail.com>
2022-06-21 15:14:49 +0530

    1st upload beta testing on testflight

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -1,7 +1,7 @@
 import { CapacitorConfig } from '@capacitor/cli';
 
 const config: CapacitorConfig = {
-  appId: 'io.ionic.starter',
+  appId: 'click.buyandsell.shop',
   appName: 'Buyandsell',
   webDir: 'www',
   bundledWebRuntime: false
